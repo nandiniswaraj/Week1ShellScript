20576d96a9e91447308ce86930535b7a744f6a89
nandiniswaraj <61447397+nandiniswaraj@users.noreply.github.com>
2020-04-26 15:13:46 -0400

    Update Primefactors.sh

--- Programing Construct/Repetition/ForLoopStatement/Primefactors.sh
+++ Programing Construct/Repetition/ForLoopStatement/Primefactors.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -x
 read -p "Enter the n " n
-
-	for(( i=2; i<=$n; i++ ))
-	do
-		while (($n%$i==0))
-		do
-			primeFactor=$primeFactor$i" ";
-			n=$((n/i))
-		done
-        done
+for(( i=2; i<=$n; i++ ))
+do
+while (($n%$i==0))
+do
+    primeFactor=$primeFactor$i" ";
+     n=$((n/i))
+done
+done
 echo $primeFactor
