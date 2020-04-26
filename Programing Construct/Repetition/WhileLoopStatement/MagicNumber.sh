a5be28cbc0d86b996cfb911504a334999bec1024
nandiniswaraj <61447397+nandiniswaraj@users.noreply.github.com>
2020-04-26 15:19:12 -0400

    Update MagicNumber.sh

--- Programing Construct/Repetition/WhileLoopStatement/MagicNumber.sh
+++ Programing Construct/Repetition/WhileLoopStatement/MagicNumber.sh
@@ -3,18 +3,18 @@
 low=1;
 high=1000;
 while (( $high>$low ))
-   do
+do
       mid=$(( $(($low+$high))/2 ));
-        echo "press 1 for guess the number is lies between $low and $mid else press 2 ";
-       read -p "input is : " input
-        if [ $(($high-$low)) -eq 1 ]
-          then   echo $high;
-                 break;
-        fi
-        if [ $input -eq 1 ]
-        then
-              high=$mid;
-        else
-              low=$mid;
-        fi
-   done
+      echo "press 1 for guess the number is lies between $low and $mid else press 2 ";
+      read -p "input is : " input
+if [ $(($high-$low)) -eq 1 ]
+then   echo $high;
+       break;
+fi
+if [ $input -eq 1 ]
+then
+       high=$mid;
+else
+       low=$mid;
+fi
+done
