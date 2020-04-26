03bfbae1e285a000d560503fe95a89aa4b7f27b8
nandiniswaraj <61447397+nandiniswaraj@users.noreply.github.com>
2020-04-26 15:35:55 -0400

    Update SumThreenum.sh

--- Arrays/SumThreenum.sh
+++ Arrays/SumThreenum.sh
@@ -5,30 +5,30 @@ read n
 fillarray(){
 for((i=0;i<n;i++))
 do
-read -p 'enter the value ' value 
-array[$i]=$value
+    read -p 'enter the value ' value 
+    array[$i]=$value
 done
 }
 display(){
 for((i=0;i<n;i++))
 do
-echo -e "${array[$i]} \c"
+   echo -e "${array[$i]} \c"
 done
 }
 threeNumberSumZero(){
 for((i=0;i<n;i++))
 do
-	for((j=0;j<n;j++))
-	    do
-   		  for((k=0;k<n;k++))
-	            do
-                         z=$(( ${array[$i]}+ ${array[$j]}+ ${array[$k]} ))
-                      if [ "$z" == 0 ]
-			then
-			   echo "Triplet value is ${array[$i]},${array[$j]},${array[$k]}"
-				fi
-                        done
-                 done
+for((j=0;j<n;j++))
+do
+for((k=0;k<n;k++))
+do
+     z=$(( ${array[$i]}+ ${array[$j]}+ ${array[$k]} ))
+if [ "$z" == 0 ]
+then 
+    echo "Triplet value is ${array[$i]},${array[$j]},${array[$k]}"
+fi
+done
+done
 done
 }
 fillarray
