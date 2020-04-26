e4554a2bfc66b0fb74596b14802dccfe20d20e17
nandiniswaraj <61447397+nandiniswaraj@users.noreply.github.com>
2020-04-26 15:17:10 -0400

    Update GamblerSimulator.sh

--- Programing Construct/Repetition/WhileLoopStatement/GamblerSimulator.sh
+++ Programing Construct/Repetition/WhileLoopStatement/GamblerSimulator.sh
@@ -7,24 +7,24 @@ win=0
 los=0
 while(($stake>0))&&(($stake!=$goal))&&(($n>0))
 do
-	echo "Enter the bet amount"
-	read bet
-	if (($bet>$stake))
-	then
-		echo "bet is more than stake"
-		continue
-	fi
-	r=0.$(($RANDOM % 10))
-	if (($(awk -v x=$r -v y=0.5 'BEGIN {print(x>y)}')))
-	then
-		echo "You won the bet"
-		stake=$((stake+bet))
-		win=$((win+1))
-	else
-		echo "You lost the bet"
-		stake=$((stake-bet))
-		los=$((los+1))
-	fi
+   echo "Enter the bet amount"
+   read bet
+if (($bet>$stake))
+then
+    echo "bet is more than stake"
+    continue
+fi
+    r=0.$(($RANDOM % 10))
+if (($(awk -v x=$r -v y=0.5 'BEGIN {print(x>y)}')))
+then
+	echo "You won the bet"
+	stake=$((stake+bet))
+	win=$((win+1))
+else
+	echo "You lost the bet"
+	stake=$((stake-bet))
+	los=$((los+1))
+fi
 	echo "Stake: $stake"
 	n=$((n-1))
 done
