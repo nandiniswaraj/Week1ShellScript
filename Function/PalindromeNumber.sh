604e576febb170a883e1c12f29cd4bc2afb9bf31
nandiniswaraj <61447397+nandiniswaraj@users.noreply.github.com>
2020-04-26 15:21:10 -0400

    Update PalindromeNumber.sh

--- Function/PalindromeNumber.sh
+++ Function/PalindromeNumber.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -x
  palindromeNum() {
-	num=$1;
-	tem=$num;
-	sum=0;
-	while (( $num > 0 ))
-	do
-		rem=$(($num%10));
-		sum=$(($sum*10+$rem));
-		num=$(($num/10));
-	done
+num=$1;
+tem=$num;
+sum=0;
+while (( $num > 0 ))
+do
+	rem=$(($num%10));
+	sum=$(($sum*10+$rem));
+	num=$(($num/10));
+done
 	echo $sum
 }
 read -p "Enter the num to print its palindromeNum " num
