3d2eb194b85d06d09210e89f9b7d68cb58ff1eda
nandiniswaraj <61447397+nandiniswaraj@users.noreply.github.com>
2020-04-26 12:03:53 -0400

    Update ArithMatic.sh

--- Programing Construct/Sequence/ArithMatic.sh
+++ Programing Construct/Sequence/ArithMatic.sh
@@ -2,11 +2,11 @@
 read -p "Enter the number" a
 read -p "Enter the number" b
 read -p "Ente the number" c
-operation1=$(($a+$b*$c));
-operation2=$(($a%$b+$c));
-operation3=$(($c+$a/$b));
-opertion4=$(($a*$b+$c));
-echo $operation1;
-echo $operation2;
-echo $opaertion3;
-echo $operation4;
+expression1=$(($a+$b*$c));
+expression2=$(($a%$b+$c));
+expression3=$(($c+$a/$b));
+expression4=$(($a*$b+$c));
+echo $expression1;
+echo $expression2;
+echo $expression3;
+echo $expression4;
