9b5d1f35d4a59f1bd232e852403a860bd374d0c1
nandiniswaraj <61447397+nandiniswaraj@users.noreply.github.com>
2020-04-26 14:47:46 -0400

    Update NumberWord.sh

--- Programing Construct/Selection/NumberWord.sh
+++ Programing Construct/Selection/NumberWord.sh
@@ -5,28 +5,28 @@ then
 echo "One";
 elif [ $digit -eq 2 ];
 then
-	echo "Two";
-	elif [ $digit -eq 3 ];
-	then
-		echo "Three";
-		elif [ $digit -eq 4 ];
-		then
-			echo "Four";
-			elif [ $digit -eq 5 ];
-			then
-					echo "Five";
-			elif [ $digit -eq 6 ];
-			then
-					echo "Six";
-					elif [ $digit -eq 7 ];
-					then
-						echo "Seven";
-						elif [ $digit -eq 8 ];
-						then
-							echo "Eight";
-							elif [ $digit -eq 9 ];
-							then
-								echo "Nine";
-								else
-								echo "Invalid input";
-							        fi
+     echo "Two";
+elif [ $digit -eq 3 ];
+then
+     echo "Three";
+elif [ $digit -eq 4 ];
+then
+     echo "Four";
+elif [ $digit -eq 5 ];
+then
+      echo "Five";
+elif [ $digit -eq 6 ];
+then
+	echo "Six";
+elif [ $digit -eq 7 ];
+then
+      echo "Seven";
+elif [ $digit -eq 8 ];
+then
+     necho "Eight";
+elif [ $digit -eq 9 ];
+then
+      echo "Nine";
+else
+      echo "Invalid input";
+ fi
