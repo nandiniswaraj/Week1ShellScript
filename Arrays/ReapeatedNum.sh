2303518dea06e3812043c4aa64a3ae0384a38b67
nandiniswaraj <61447397+nandiniswaraj@users.noreply.github.com>
2020-04-26 15:33:56 -0400

    Update ReapeatedNum.sh

--- Arrays/ReapeatedNum.sh
+++ Arrays/ReapeatedNum.sh
@@ -2,13 +2,13 @@
 j=0;
 for (( i=1; i<100; i++ ))
 do
-   	  num=$i;
-	     digit=$(($num%10));
+   	 num=$i;
+	 digit=$(($num%10));
     	 num=$(($num/10));
-   	  if (( $num == $digit ))
-   	  then
-      	   arr[j]=$i;
-         	 ((j++));
-   		 fi
+if (( $num == $digit ))
+then
+      	  arr[j]=$i;
+           ((j++));
+fi
 done
 echo The Number with repeated digits are ${arr[@]}
