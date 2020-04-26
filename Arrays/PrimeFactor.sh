843e71a396a5f89131d420011381b67d5c7e3984
nandiniswaraj <61447397+nandiniswaraj@users.noreply.github.com>
2020-04-26 15:28:46 -0400

    Update PrimeFactor.sh

--- Arrays/PrimeFactor.sh
+++ Arrays/PrimeFactor.sh
@@ -2,24 +2,24 @@
 read -p "Enter n: " num;
 i=0;
 while (( $num%2==0 ))
-   do
+do
        arrayPrFac[i]=2;
        num=$(( $num / 2));
        ((i++));
-   done
+done
 for (( j=3; $j*$j<=$num; j=$j+2 ))
-     do
-         while (( $num%$j==0 ))
-           do
-               arrayPrFac[i]=$j;
-               num=$(( $num / $j));
-               ((i++));
-           done
-     done
+do
+while (( $num%$j==0 ))
+do
+         arrayPrFac[i]=$j;
+         num=$(( $num / $j));
+         ((i++));
+done
+done
 if (( $num >= 2 ))
 then
-    arrayPrFac[i]=$num;
+        arrayPrFac[i]=$num;
 fi
-echo given the Prime Factor is ${arrayPrFac[@]};
+       echo given the Prime Factor is ${arrayPrFac[@]};
 
 
