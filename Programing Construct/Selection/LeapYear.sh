cac1cc5257a01980003bb56f4458b3650ccbc47c
nandiniswaraj <61447397+nandiniswaraj@users.noreply.github.com>
2020-04-26 14:59:16 -0400

    Update LeapYear.sh

--- Programing Construct/Selection/LeapYear.sh
+++ Programing Construct/Selection/LeapYear.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
-read y
-if (($y%400==0 ))
+echo "enter year"
+read year
+if (($year%400==0 ))
 then 
-echo  " $y is leap year";
-elif (($y%4==0)) && (($y%100!=0))
+echo  " $year is leap year";
+elif (($year%4==0)) && (($year%100!=0))
 then
-	echo "$y is Leap Year"
+	echo "$year is Leap Year"
 else
-	echo "$y is not a Leap Year"
+	echo "$year is not a Leap Year"
 fi
